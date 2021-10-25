d42136a8feaa5b1592e983d38d4d5893895c64c6
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-10-25 01:01:10 -0700

    everything to processor part 1

---
+++ src/entrypoint.rs
@@ -0,0 +1,26 @@
+use solana_program::{
+    account_info::AccountInfo, 
+    entrypoint, 
+    entrypoint::ProgramResult, 
+    msg, 
+    pubkey::Pubkey,
+};
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+// Program entrypoint's implementation
+fn process_instruction(
+    program_id: &Pubkey, // Public key of the account the program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+) -> ProgramResult {
+    msg!(
+        "process_instruction: {}: {} accounts, data={:?}",
+        program_id,
+        accounts.len(),
+        instruction_data
+    );
+    Ok(())
+}
+
